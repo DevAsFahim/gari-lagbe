4063006cd7470e0e338c041da53837711a4b441c
DevAsFahim <108362901+DevAsFahim@users.noreply.github.com>
2026-04-13 19:38:20 +0600

    create user

---
+++ src/app/api/auth/register/route.ts
@@ -0,0 +1,34 @@
+import connectDB from "@/lib/db";
+import User from "@/models/user.model";
+import bcrypt from "bcryptjs";
+import { NextRequest, NextResponse } from "next/server";
+
+export async function POST(req: NextRequest) {
+  try {
+    const { name, email, password } = await req.json();
+    await connectDB();
+
+    let user = await User.findOne({ email });
+    if (user) {
+      return NextResponse.json(
+        { message: "Email already exist!" },
+        { status: 400 },
+      );
+    }
+
+    const hashedPassword = await bcrypt.hash(password, 10);
+
+    user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+    });
+
+    return NextResponse.json(user, { status: 201 });
+  } catch (error) {
+    return NextResponse.json(
+      { message: `Register error ${error}` },
+      { status: 400 },
+    );
+  }
+}
