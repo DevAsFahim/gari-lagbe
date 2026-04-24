88c2b89264d75660be85add2db8e24f5b3493a44
DevAsFahim <devasfahim@gmail.com>
2026-04-24 11:27:00 +0600

    login

--- src/components/AuthModal.tsx
+++ src/components/AuthModal.tsx
@@ -1,6 +1,6 @@
-import React from "react";
+import React, { useState } from "react";
 import { motion } from "motion/react";
-import { X } from "lucide-react";
+import { Lock, Mail, X } from "lucide-react";
 import Image from "next/image";
 
 interface IProps {
@@ -8,7 +8,10 @@ interface IProps {
   onClose: () => void;
 }
 
+type stepType = "login" | "signup" | "otp";
 const AuthModal = ({ open, onClose }: IProps) => {
+  const [step, setStep] = useState<stepType>("login");
+
   return (
     <>
       {open && (
@@ -40,9 +43,59 @@ const AuthModal = ({ open, onClose }: IProps) => {
               </div>
 
               <button className="w-full h-11 rounded-xl border border-black/20 flex items-center justify-center gap-3 text-sm font-semibold hover:bg-black hover:text-white transition cursor-pointer">
-              <Image src="/google.png" width={20} height={20} alt="google"/>
-              Continue with Google
+                <Image src="/google.png" width={20} height={20} alt="google" />
+                Continue with Google
               </button>
+
+              <div className="flex items-center gap-4 my-6">
+                <div className="flex-1 h-px bg-black/10" />
+                <span className="text-xs text-gray-500">OR</span>
+                <div className="flex-1 h-px bg-black/10" />
+              </div>
+
+              {/* {step === 'login' && <Login />} */}
+              {step === "login" && (
+                <motion.div
+                  initial={{ opacity: 0, x: 20 }}
+                  animate={{ opacity: 1, x: 0 }}
+                >
+                  <h1 className="text-xl font-semibold">Welcome Back</h1>
+
+                  <div className="mt-5 space-y-4">
+                    <div className="flex items-center gap-3 border border-black/20 rounded-xl px-4 py-3">
+                      <Mail size={18} className="text-gray-500" />
+                      <input
+                        type="email"
+                        placeholder="Email"
+                        className="w-full bg-transparent outline-none text-sm"
+                      />
+                    </div>
+
+                    <div className="flex items-center gap-3 border border-black/20 rounded-xl px-4 py-3">
+                      <Lock size={18} className="text-gray-500" />
+                      <input
+                        type="password"
+                        placeholder="Password"
+                        className="w-full bg-transparent outline-none text-sm"
+                      />
+                    </div>
+
+                    <button className="w-full h-11 rounded-xl bg-black text-white font-semibold hover:bg-gray-900 transition cursor-pointer">
+                      Login
+                    </button>
+                  </div>
+
+                  <p className="mt-6 text-center text-sm text-gray-500">
+                    Don't have an account?
+                    <span
+                      className="text-black hover:underline cursor-pointer font-medium block"
+                      onClick={() => setStep("signup")}
+                    >
+                      Sign Up
+                    </span>
+                  </p>
+                </motion.div>
+              )}
             </div>
           </motion.div>
         </>
